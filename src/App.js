ae971589fe39dec70ba39ffb31177dae19c21e7e
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-02 12:13:05 -0300

    init setting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+const MostraVoltas = ({voltas}) => {
+  return (
+    <p>10<br/>Voltas: {voltas}</p>
+  )
+
+}
+const MostraTempo = ({tempo}) => {
+  return (
+    <p>Tempo médio de voltas: {tempo}</p>
+  )
+
+}
+
+const Button = ({text}) => <button>{text}</button>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Contador-voltas</h1>
+      <MostraVoltas voltas='45'/>
+      
+      <Button text='+'/>
+      <Button text='-'/>
+     <MostraTempo tempo='1:30'/>
+     <Button text='Iniciar'/>
+     <Button text='Reiniciar'/>
     </div>
-  );
+  )
 }
 
 export default App;
