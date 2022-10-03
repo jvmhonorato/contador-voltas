62a5873a76fbd9de32d3d2a1b770e5f2294c0849
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-03 11:11:09 -0300

    Using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React ,{useState, useEffect} from "react";
 
 
 const MostraVoltas = ({voltas}) => {
   return (
-    <p>10<br/>Voltas: {voltas}</p>
+    <p>Voltas: {voltas}</p>
   )
 
 }
@@ -14,17 +14,39 @@ const MostraTempo = ({tempo}) => {
 
 }
 
-const Button = ({text}) => <button>{text}</button>
+//put event onClick as a props before send to component
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 function App() {
+// current state, change state  =  start state
+const [numVoltas, setNumVoltas] = useState(10)
+const [tempo, setTempo] = useState(0)
+
+useEffect(() => {
+  setInterval(() => {
+    console.log('chamou')
+  },1000)
+
+}, [])
+
+
+  
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+    console.log('increment')
+  }
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1)
+    console.log('decrement')
+  }
   return (
     <div className='App'>
       <h1>Contador-voltas</h1>
-      <MostraVoltas voltas='45'/>
+      <MostraVoltas voltas={numVoltas}/>
       
-      <Button text='+'/>
-      <Button text='-'/>
-     <MostraTempo tempo='1:30'/>
+      <Button text='+' onClick={increment}/>
+      <Button text='-' onClick={decrement}/>
+     <MostraTempo tempo={tempo}/>
      <Button text='Iniciar'/>
      <Button text='Reiniciar'/>
     </div>
